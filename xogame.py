23c96cf77ae028dd10f1fd25a6e0008065c99d8a
halitekmekcioglu <halitekmekci@msn.com>
2022-10-13 23:12:56 +0300

    New Repo

---
+++ xogame.py
@@ -0,0 +1,48 @@
+"""
+X - O game 
+it counts 3 times X or O's
+if any side has 3 points, it wins.
+If both side has 3 points or higher, it's tie.
+If both side has no points,it's tie
+"""
+
+A = "OXOOXOXOXOXOOOOX"
+x = []
+o = []
+
+
+def sol(x, o, A):
+
+    a = [*A]
+    z = 0
+    w = 0
+    for i in range(0, (len(a))):
+        for k in range(1, (len(a))):
+
+            if a[i] == a[k] and abs(i-k) <= 2:
+                z = z+1
+                w = w+1
+                #print("z", z, "i", i, "k", k)
+                if z >= 3:
+                    if a[k] == "X":
+                        x.append(a[k])
+
+                if w >= 3:
+                    if a[k] == "O":
+                        o.append(a[k])
+
+            else:
+                z = 0
+                w = 0
+                #print("x", x, "o:", o)
+                pass
+
+    if len(x) >= 3 and len(o) < 3:
+        return "X is winner", x, o
+    elif len(x) < 3 and len(o) > 3:
+        return "O is winner", x, o
+    elif len(x) >= 3 and len(o) >= 3 or len(x) < 3 and len(o) < 3:
+        return "its tie", x, o
+
+
+print(sol(x, o, A))
